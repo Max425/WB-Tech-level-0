287e9099e2000d37e9e541c808779d4507de175f
Maxim Sikanov <max.4251474@yandex.ru>
2024-02-07 23:11:17 +0300

    Add entry point for nats server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/Max425/WB-Tech-level-0/cmd"
-	"github.com/Max425/WB-Tech-level-0/cmd/nats"
 	"github.com/Max425/WB-Tech-level-0/pkg/api"
 	"github.com/Max425/WB-Tech-level-0/pkg/api/handler"
+	"github.com/Max425/WB-Tech-level-0/pkg/nats"
 	"github.com/Max425/WB-Tech-level-0/pkg/repository"
 	"github.com/Max425/WB-Tech-level-0/pkg/service"
-	"github.com/nats-io/stan.go"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"log"
@@ -56,14 +55,13 @@ func main() {
 	go func() {
 		if err = srv.Serve(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 			logger.Error("error occurred on server shutting down", zap.Error(err))
-			os.Exit(1)
 		}
 	}()
 	err = services.Order.LoadOrdersToCache(ctx)
 	if err != nil {
 		logger.Error("error load cache", zap.Error(err))
 	}
-	go startNats(logger)
+	go nats.StartNatsClient(ctx, logger, services)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
@@ -74,19 +72,4 @@ func main() {
 	if err = srv.Shutdown(context.Background()); err != nil {
 		logger.Error("error occurred on server shutting down: %s", zap.Error(err))
 	}
-}
-
-func startNats(logger *zap.Logger) {
-	sc, err := stan.Connect(
-		viper.GetString("nats.clusterID"),
-		viper.GetString("nats.clientID"),
-		stan.NatsURL(viper.GetString("nats.url")),
-	)
-	if err != nil {
-		logger.Error("Error stan Connect", zap.Error(err))
-		return
-	}
-	natsWorker := nats.NewNats(sc, logger)
-	go natsWorker.StartClient()
-	go natsWorker.StartServer()
 }
